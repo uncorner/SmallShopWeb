5165363d952315da1e5853535d9713c7943916f9
uncorner <denis.uncorner@gmail.com>
2023-07-29 20:37:02 +0300

    add FacadeController

---
+++ SmallShopWeb/ShopAPI/App/Controllers/FacadeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopAPI.App.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class FacadeController : ControllerBase
+    {
+
+        [HttpGet("info")]
+        public IActionResult GetInfo()
+        {
+            return Ok("API info");
+        }
+
+    }
+}
