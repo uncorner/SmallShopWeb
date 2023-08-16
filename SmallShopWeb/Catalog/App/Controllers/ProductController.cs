2cb33eda033c1615d97b0f3f82f74e5ba8f51b3c
uncorner <denis.uncorner@gmail.com>
2023-08-16 12:53:29 +0300

    add grpc service

--- SmallShopWeb/Catalog/App/Controllers/ProductController.cs
+++ SmallShopWeb/Catalog/App/Controllers/ProductController.cs
@@ -1,109 +1,109 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using SmallShopWeb.Catalog.App.Dto;
-using SmallShopWeb.Catalog.App.Entities;
-using SmallShopWeb.Catalog.App.Repository;
-using SmallShopWeb.ShopCommon.Dto;
-using System.Collections.Immutable;
-using System.Net;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using SmallShopWeb.Catalog.App.Dto;
+//using SmallShopWeb.Catalog.App.Entities;
+//using SmallShopWeb.Catalog.App.Repository;
+//using SmallShopWeb.ShopCommon.Dto;
+//using System.Collections.Immutable;
+//using System.Net;
 
-namespace SmallShopWeb.Catalog.App.Controllers
-{
-    [Route("api")]
-    [ApiController]
-    public class ProductController : ControllerBase
-    {
-        private readonly IUnitOfWorkFactory unitOfWorkFactory;
+//namespace SmallShopWeb.Catalog.App.Controllers
+//{
+//    [Route("api")]
+//    [ApiController]
+//    public class ProductController : ControllerBase
+//    {
+//        private readonly IUnitOfWorkFactory unitOfWorkFactory;
 
-        public ProductController(IUnitOfWorkFactory unitOfWorkFactory)
-        {
-            this.unitOfWorkFactory = unitOfWorkFactory;
-        }
+//        public ProductController(IUnitOfWorkFactory unitOfWorkFactory)
+//        {
+//            this.unitOfWorkFactory = unitOfWorkFactory;
+//        }
 
-        [HttpGet("products")]
-        public async Task<IActionResult> GetProducts()
-        {
-            using var unitOfWork = unitOfWorkFactory.CreateUnitOfWork();
-            var productRepository = unitOfWork.CreateProductRepository();
+//        [HttpGet("products")]
+//        public async Task<IActionResult> GetProducts()
+//        {
+//            using var unitOfWork = unitOfWorkFactory.CreateUnitOfWork();
+//            var productRepository = unitOfWork.CreateProductRepository();
 
-            var products = await productRepository.GetAllAsync();
+//            var products = await productRepository.GetAllAsync();
 
-            var result = products.Select(p =>
-                new ProductInfo(p.Id, p.Name, p.Description ?? "", p.Price)).ToArray();
+//            var result = products.Select(p =>
+//                new ProductInfo(p.Id, p.Name, p.Description ?? "", p.Price)).ToArray();
 
-            return Ok(result);
-        }
+//            return Ok(result);
+//        }
 
-        [HttpPost("products")]
-        public async Task<IActionResult> CreateProducts([FromBody] CreateProductData[] datas)
-        {
-            using var unitOfWork = unitOfWorkFactory.CreateUnitOfWork();
-            var productRepository = unitOfWork.CreateProductRepository();
+//        [HttpPost("products")]
+//        public async Task<IActionResult> CreateProducts([FromBody] CreateProductData[] datas)
+//        {
+//            using var unitOfWork = unitOfWorkFactory.CreateUnitOfWork();
+//            var productRepository = unitOfWork.CreateProductRepository();
 
-            var products = datas.Select(i => new Product(i.Name)
-            {
-                Description = i.Description,
-                Price = i.Price
-            }).ToArray();
+//            var products = datas.Select(i => new Product(i.Name)
+//            {
+//                Description = i.Description,
+//                Price = i.Price
+//            }).ToArray();
 
-            await productRepository.AddRange(products);
-            await unitOfWork.SaveChangesAsync();
+//            await productRepository.AddRange(products);
+//            await unitOfWork.SaveChangesAsync();
 
-            var ids = products.Select(i => i.Id).ToArray();
-            return Ok(ids);
-        }
+//            var ids = products.Select(i => i.Id).ToArray();
+//            return Ok(ids);
+//        }
 
-        [HttpPut("products")]
-        public async Task<IActionResult> UpdateProducts([FromBody] UpdateProductData[] datas)
-        {
-            using var unitOfWork = unitOfWorkFactory.CreateUnitOfWork();
-            var productRepository = unitOfWork.CreateProductRepository();
+//        [HttpPut("products")]
+//        public async Task<IActionResult> UpdateProducts([FromBody] UpdateProductData[] datas)
+//        {
+//            using var unitOfWork = unitOfWorkFactory.CreateUnitOfWork();
+//            var productRepository = unitOfWork.CreateProductRepository();
 
-            var ids = datas.Select(i => i.Id).ToArray();
-            var products = await productRepository.GetByIds(ids);
-            var productsMap = products.ToDictionary(i => i.Id);
+//            var ids = datas.Select(i => i.Id).ToArray();
+//            var products = await productRepository.GetByIds(ids);
+//            var productsMap = products.ToDictionary(i => i.Id);
 
-            foreach (var data in datas)
-            {
-                if (productsMap.TryGetValue(data.Id, out Product? product))
-                {
-                    product.Name = data.Name;
-                    product.Description = data.Description;
-                    product.Price = data.Price;
-                }
-                else
-                {
-                    return StatusCode((int)HttpStatusCode.NotFound,
-                        $"Product with id {data.Id} is not found");
-                }
-            }
+//            foreach (var data in datas)
+//            {
+//                if (productsMap.TryGetValue(data.Id, out Product? product))
+//                {
+//                    product.Name = data.Name;
+//                    product.Description = data.Description;
+//                    product.Price = data.Price;
+//                }
+//                else
+//                {
+//                    return StatusCode((int)HttpStatusCode.NotFound,
+//                        $"Product with id {data.Id} is not found");
+//                }
+//            }
 
-            await unitOfWork.SaveChangesAsync();
-            return Ok();
-        }
+//            await unitOfWork.SaveChangesAsync();
+//            return Ok();
+//        }
 
-        [HttpDelete("products")]
-        public async Task<IActionResult> RemoveProducts([FromBody] int[] productIds)
-        {
-            productIds = productIds.Distinct().ToArray();
+//        [HttpDelete("products")]
+//        public async Task<IActionResult> RemoveProducts([FromBody] int[] productIds)
+//        {
+//            productIds = productIds.Distinct().ToArray();
 
-            using var unitOfWork = unitOfWorkFactory.CreateUnitOfWork();
-            var productRepository = unitOfWork.CreateProductRepository();
-            var existedIds = await productRepository.CheckProductsExist(productIds);
+//            using var unitOfWork = unitOfWorkFactory.CreateUnitOfWork();
+//            var productRepository = unitOfWork.CreateProductRepository();
+//            var existedIds = await productRepository.CheckProductsExist(productIds);
 
-            if (productIds.Length != existedIds.Count())
-            {
-                foreach(var id in productIds) {
-                    if (!existedIds.Contains(id))
-                    {
-                        return StatusCode((int)HttpStatusCode.NotFound,
-                            $"Product with id {id} is not found");
-                    }
-                }
-            }
+//            if (productIds.Length != existedIds.Count())
+//            {
+//                foreach(var id in productIds) {
+//                    if (!existedIds.Contains(id))
+//                    {
+//                        return StatusCode((int)HttpStatusCode.NotFound,
+//                            $"Product with id {id} is not found");
+//                    }
+//                }
+//            }
 
-            await productRepository.BatchRemove(productIds);
-            return Ok();
-        }
+//            await productRepository.BatchRemove(productIds);
+//            return Ok();
+//        }
 
-    }
-}
+//    }
+//}
--- SmallShopWeb/Catalog/Program.cs
+++ SmallShopWeb/Catalog/Program.cs
@@ -1,26 +1,45 @@
-using Microsoft.EntityFrameworkCore;
-using SmallShopWeb.Catalog.App;
-using SmallShopWeb.Catalog.Infrastructure;
+//using Microsoft.EntityFrameworkCore;
+//using SmallShopWeb.Catalog.App;
+//using SmallShopWeb.Catalog.Infrastructure;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+
+//var connectionString = builder.Configuration.GetConnectionString("Default")
+//    ?? throw new NullReferenceException("Cannot get connection string");
+//builder.Services.AddDbContextFactory<ApplicationDbContext>(opt => opt.UseNpgsql(connectionString));
+//builder.Services.AddCustomServices();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//// todo: turn on https (check launchSettings.json)
+////app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
+using SmallShopWeb.Catalog.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
 // Add services to the container.
-
-builder.Services.AddControllers();
-
-var connectionString = builder.Configuration.GetConnectionString("Default")
-    ?? throw new NullReferenceException("Cannot get connection string");
-builder.Services.AddDbContextFactory<ApplicationDbContext>(opt => opt.UseNpgsql(connectionString));
-builder.Services.AddCustomServices();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// todo: turn on https (check launchSettings.json)
-//app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapGrpcService<TranslatorService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
