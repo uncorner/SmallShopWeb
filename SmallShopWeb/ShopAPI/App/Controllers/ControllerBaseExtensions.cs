c68405149808919cb77b27fed84be45af603a52f
uncorner <denis.uncorner@gmail.com>
2023-08-23 13:35:06 +0300

    add HandleErrors to controller

---
+++ SmallShopWeb/ShopAPI/App/Controllers/ControllerBaseExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Core;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace SmallShopWeb.ShopAPI.App.Controllers
+{
+    internal static class ControllerBaseExtensions
+    {
+        public static async Task<IActionResult> HandleErrors(this ControllerBase controller,
+            ILogger logger,
+            Func<Task<IActionResult>> processRequest)
+        {
+            try
+            {
+                return await processRequest();
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.NotFound)
+            {
+                logger.LogError(ex.Message);
+                return controller.StatusCode((int)HttpStatusCode.NotFound, ex.Status.Detail);
+            }
+            catch (Exception ex)
+            {
+                logger.LogCritical(ex.Message);
+                return controller.StatusCode((int)HttpStatusCode.InternalServerError, "Something went wrong");
+            }
+        }
+        
+
+    }
+}
--- SmallShopWeb/ShopAPI/App/Controllers/ProductController.cs
+++ SmallShopWeb/ShopAPI/App/Controllers/ProductController.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SmallShopWeb.ShopCommon.Dto;
-using System.Net;
 using SmallShopWeb.ShopAPI.App.Client;
 using CreateProductData = SmallShopWeb.Catalog.App.Dto.CreateProductData;
 using UpdateProductData = SmallShopWeb.Catalog.App.Dto.UpdateProductData;
-using Grpc.Core;
 
 namespace SmallShopWeb.ShopAPI.App.Controllers
 {
@@ -21,34 +19,24 @@ namespace SmallShopWeb.ShopAPI.App.Controllers
             this.logger = logger;
         }
 
-        [HttpGet("info")]
-        public IActionResult GetInfo()
-        {
-            return Ok("API info");
-        }
-
         [HttpGet("products")]
         public async Task<IActionResult> GetProducts()
         {
-            try {
+            return await this.HandleErrors(logger, async () =>
+            {
                 var listReply = await catalogClient.GetProductsAsync();
 
                 var productInfos = listReply.Products.Select(p =>
                     new ProductInfo(p.Id, p.Name, p.Description, p.Price)).ToArray();
 
                 return Ok(productInfos);
-            }
-            catch (Exception ex)
-            {
-                logger.LogCritical(ex.Message);
-                return StatusCode((int)HttpStatusCode.InternalServerError, "Something went wrong");
-            }
+            });
         }
 
         [HttpPost("products")]
         public async Task<IActionResult> CreateProducts([FromBody] CreateProductData[] datas)
         {
-            try
+            return await this.HandleErrors(logger, async () =>
             {
                 var productDatas = datas.Select(i => new Client.CreateProductData()
                 {
@@ -63,18 +51,13 @@ namespace SmallShopWeb.ShopAPI.App.Controllers
                 var result = await catalogClient.CreateProductsAsync(request);
 
                 return Ok(result.Ids.ToArray());
-            }
-            catch (Exception ex)
-            {
-                logger.LogCritical(ex.Message);
-                return StatusCode((int)HttpStatusCode.InternalServerError, "Something went wrong");
-            }
+            });
         }
 
         [HttpPut("products")]
         public async Task<IActionResult> UpdateProducts([FromBody] UpdateProductData[] datas)
         {
-            try
+            return await this.HandleErrors(logger,async () =>
             {
                 var productDatas = datas.Select(i => new Client.UpdateProductData()
                 {
@@ -90,42 +73,22 @@ namespace SmallShopWeb.ShopAPI.App.Controllers
                 var result = await catalogClient.UpdateProductsAsync(request);
 
                 return Ok();
-            }
-            catch(RpcException ex) when (ex.StatusCode == Grpc.Core.StatusCode.NotFound)
-            {
-                logger.LogError(ex.Message);
-                return StatusCode((int)HttpStatusCode.NotFound, ex.Status.Detail);
-            }
-            catch(Exception ex)
-            {
-                logger.LogCritical(ex.Message);
-                return StatusCode((int)HttpStatusCode.InternalServerError, "Something went wrong");
-            }
+            });
         }
 
         [HttpDelete("products")]
         public async Task<IActionResult> RemoveProducts([FromBody] int[] productIds)
         {
-            try
+            return await this.HandleErrors(logger, async () =>
             {
                 var request = new RemoveProductsRequest();
                 request.Ids.AddRange(productIds);
 
                 var result = await catalogClient.RemoveProductsAsync(request);
                 return Ok();
-            }
-            catch (RpcException ex) when (ex.StatusCode == Grpc.Core.StatusCode.NotFound)
-            {
-                logger.LogError(ex.Message);
-                return StatusCode((int)HttpStatusCode.NotFound, ex.Status.Detail);
-            }
-            catch (Exception ex)
-            {
-                logger.LogCritical(ex.Message);
-                return StatusCode((int)HttpStatusCode.InternalServerError, "Something went wrong");
-            }
+            });
         }
-
+        
 
     }
 }
