0a34df883550f674b352136a9a2b3647d68e424a
uncorner <denis.uncorner@gmail.com>
2023-11-27 18:51:17 +0300

    edit views

--- SmallShopWeb/ShopWeb/Presentation/Models/ErrorViewModel.cs
+++ SmallShopWeb/ShopWeb/Presentation/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace SmallShopWeb.ShopWeb.Presentation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace SmallShopWeb.ShopWeb.Presentation.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
